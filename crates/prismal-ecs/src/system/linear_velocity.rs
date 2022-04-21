75361bf00dba269eb2885355e29bdbe89d8c2019
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2022-04-20 21:47:50 -0400

    Change: Use actual frame delta time in `SysLinearVelocity`

--- crates/prismal-ecs/src/system/linear_velocity.rs
+++ crates/prismal-ecs/src/system/linear_velocity.rs
@@ -2,14 +2,9 @@ use std::ops::{Deref, DerefMut};
 
 use crate::component::linear_velocity::CptLinearVelocity;
 use crate::component::position::CptPosition;
+use crate::resource::time::Time;
 
 use prismal_ecs_core::prelude::*;
-
-#[derive(SystemData)]
-pub struct SysLinearVelocityData<'a> {
-    positions: WriteStorage<'a, CptPosition>,
-    velocities: ReadStorage<'a, CptLinearVelocity>,
-}
 
 pub struct SysLinearVelocity;
 
@@ -17,11 +12,17 @@ impl SysLinearVelocity {
     pub const NAME: &'static str = "prismal_sys_linear_velocity";
 }
 
+#[derive(SystemData)]
+pub struct SysLinearVelocityData<'a> {
+    time: Read<'a, Time>,
+    positions: WriteStorage<'a, CptPosition>,
+    velocities: ReadStorage<'a, CptLinearVelocity>,
+}
+
 impl<'a> System<'a> for SysLinearVelocity {
     type SystemData = SysLinearVelocityData<'a>;
     fn run(&mut self, mut data: Self::SystemData) {
-        // TODO: Use actual frame delta time
-        let frame_delta: f32 = 0.01667;
+        let frame_delta = data.time.frame_delta().as_secs_f32();
         for (pos, vel) in (&mut data.positions, &data.velocities).join() {
             *pos.deref_mut() += (*vel.deref()) * frame_delta;
         }
