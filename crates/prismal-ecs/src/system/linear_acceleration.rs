ffcf1271b34e1da7e8274f015fbdccb49a9cb142
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2022-04-20 21:51:17 -0400

    Change: Use actual frame delta time in `SysLinearAcceleration`

--- crates/prismal-ecs/src/system/linear_acceleration.rs
+++ crates/prismal-ecs/src/system/linear_acceleration.rs
@@ -2,11 +2,13 @@ use std::ops::{Deref, DerefMut};
 
 use crate::component::linear_acceleration::CptLinearAcceleration;
 use crate::component::linear_velocity::CptLinearVelocity;
+use crate::resource::time::Time;
 
 use prismal_ecs_core::prelude::*;
 
 #[derive(SystemData)]
 pub struct SysLinearAccelerationData<'a> {
+    time: Read<'a, Time>,
     velocities: WriteStorage<'a, CptLinearVelocity>,
     accelerations: ReadStorage<'a, CptLinearAcceleration>,
 }
@@ -20,8 +22,7 @@ impl SysLinearAcceleration {
 impl<'a> System<'a> for SysLinearAcceleration {
     type SystemData = SysLinearAccelerationData<'a>;
     fn run(&mut self, mut data: Self::SystemData) {
-        // TODO: Use actual frame delta time
-        let frame_delta: f32 = 0.01667;
+        let frame_delta = data.time.frame_delta().as_secs_f32();
         for (vel, accel) in (&mut data.velocities, &data.accelerations).join() {
             *vel.deref_mut() += (*accel.deref()) * frame_delta;
         }
