40ed2abb294c554c7ee6420f1afaea5ce55b5ab5
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2022-04-21 09:39:30 -0400

    Add: Key-based sorting for `EcsInitializer` trait

--- crates/prismal-ecs-core/src/traits/initializer.rs
+++ crates/prismal-ecs-core/src/traits/initializer.rs
@@ -1,8 +1,45 @@
 use specs::DispatcherBuilder;
 use specs::World;
 
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub enum EcsInitializeOrder {
+    User(i16),
+    #[doc(hidden)]
+    InternalUseOnlyDefaultEcsInitializer,
+}
+
+impl PartialOrd for EcsInitializeOrder {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self {
+            EcsInitializeOrder::User(rhs) => match other {
+                EcsInitializeOrder::User(lhs) => rhs.partial_cmp(lhs),
+                EcsInitializeOrder::InternalUseOnlyDefaultEcsInitializer => {
+                    Some(std::cmp::Ordering::Less)
+                }
+            },
+            EcsInitializeOrder::InternalUseOnlyDefaultEcsInitializer => match other {
+                EcsInitializeOrder::User(_) => Some(std::cmp::Ordering::Greater),
+                EcsInitializeOrder::InternalUseOnlyDefaultEcsInitializer => {
+                    Some(std::cmp::Ordering::Equal)
+                }
+            },
+        }
+    }
+}
+impl Eq for EcsInitializeOrder {}
+impl Ord for EcsInitializeOrder {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
 /// Trait used to initialize the ECS
 pub trait EcsInitializer {
+    /// Return an enum to use as a key to sort initializers before use.
+    fn get_order(&self) -> EcsInitializeOrder {
+        EcsInitializeOrder::User(0)
+    }
+
     /// Setup the tick dispatcher using a [`DispatcherBuilder`].
     /// Return the modified builder.
     ///
--- crates/prismal-ecs/src/init.rs
+++ crates/prismal-ecs/src/init.rs
@@ -1,5 +1,10 @@
+use itertools::Itertools;
+
 use prismal_ecs_core::prelude::{Dispatcher, DispatcherBuilder, World};
-use prismal_ecs_core::{prelude::WorldExt, traits::EcsInitializer};
+use prismal_ecs_core::{
+    prelude::WorldExt,
+    traits::{EcsInitializeOrder, EcsInitializer},
+};
 
 use crate::resource::time::Time;
 
@@ -40,6 +45,10 @@ impl EcsInitializer for DefaultEcsInitializer {
         world.register::<CptLinearAcceleration>();
         world.register::<CptLinearVelocity>();
     }
+
+    fn get_order(&self) -> EcsInitializeOrder {
+        EcsInitializeOrder::InternalUseOnlyDefaultEcsInitializer
+    }
 }
 
 fn default_initializers() -> Vec<Box<dyn EcsInitializer>> {
@@ -48,15 +57,14 @@ fn default_initializers() -> Vec<Box<dyn EcsInitializer>> {
 
 #[doc(hidden)]
 pub fn create_tick_dispatcher<'a, 'b>(
-    pre_initializers: &[Box<dyn EcsInitializer>],
-    post_initializers: &[Box<dyn EcsInitializer>],
+    initializers: &[Box<dyn EcsInitializer>],
 ) -> Dispatcher<'a, 'b> {
     let mut builder = DispatcherBuilder::new();
     let default_initializers = default_initializers();
-    let initializers = pre_initializers
+    let initializers = initializers
         .iter()
         .chain(default_initializers.iter())
-        .chain(post_initializers.iter());
+        .sorted_by(|a, b| a.get_order().cmp(&b.get_order()));
     for i in initializers {
         builder = i.setup_tick_dispatcher(builder);
     }
@@ -64,16 +72,13 @@ pub fn create_tick_dispatcher<'a, 'b>(
 }
 
 #[doc(hidden)]
-pub fn create_world(
-    pre_initializers: &[Box<dyn EcsInitializer>],
-    post_initializers: &[Box<dyn EcsInitializer>],
-) -> World {
+pub fn create_world(initializers: &[Box<dyn EcsInitializer>]) -> World {
     let mut world = World::new();
     let default_initializers = default_initializers();
-    let initializers = pre_initializers
+    let initializers = initializers
         .iter()
         .chain(default_initializers.iter())
-        .chain(post_initializers.iter());
+        .sorted_by(|a, b| a.get_order().cmp(&b.get_order()));
     for i in initializers {
         i.setup_world(&mut world);
     }
