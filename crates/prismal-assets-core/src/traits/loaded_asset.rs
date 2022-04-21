5a74d9a4c4723f6f3c1e15386f398f962930b6e8
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2022-04-21 13:49:45 -0400

    Add: `asset_type_id` method to `LoadedAsset` trait

--- crates/prismal-assets-core/src/traits/loaded_asset.rs
+++ crates/prismal-assets-core/src/traits/loaded_asset.rs
@@ -1,7 +1,10 @@
 use crate::id::AssetId;
+use crate::type_id::AssetTypeId;
+
 use downcast::AnySync;
 
 pub trait LoadedAsset: AnySync {
-    fn id(&self) -> AssetId;
+    fn asset_id(&self) -> AssetId;
+    fn asset_type_id(&self) -> AssetTypeId;
 }
 downcast::downcast_sync!(dyn LoadedAsset);
