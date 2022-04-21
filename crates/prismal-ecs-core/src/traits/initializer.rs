0c55290318861e7bccd80d0531e8cf9ac6d0faab
Zachary Frost <10238600+zfzackfrost@users.noreply.github.com>
2022-04-20 23:16:39 -0400

    Add: Documentation comments for `prismal-ecs-core` crate

--- crates/prismal-ecs-core/src/traits/initializer.rs
+++ crates/prismal-ecs-core/src/traits/initializer.rs
@@ -1,10 +1,17 @@
 use specs::DispatcherBuilder;
 use specs::World;
 
+/// Trait used to initialize the ECS
 pub trait EcsInitializer {
+    /// Setup the tick dispatcher using a [`DispatcherBuilder`].
+    /// Return the modified builder.
+    ///
+    /// Used to register systems that will run every frame.
     fn setup_tick_dispatcher<'a, 'b>(
         &self,
         builder: DispatcherBuilder<'a, 'b>,
     ) -> DispatcherBuilder<'a, 'b>;
+
+    /// Setup the ECS world (insert resources, register components, etc.)
     fn setup_world(&self, world: &mut World);
 }
